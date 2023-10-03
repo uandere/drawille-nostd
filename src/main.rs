a2016bc80ee13f9bf9d080f78af4d4578c62d7f2
uandere <nazar.demchvk@gmail.com>
2023-10-04 01:12:59 +0300

    Added example of drawed line

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
+use drawille_nostd::*;
 
+fn main() {
+    let mut canvas = Canvas::new(100, 100);
+
+    canvas.line(10, 15, 40, 30);
+
+    let frame = canvas.frame().replace(' ', "\u{2800}");
+
+    println!("{frame}");
 }
 
